73befc523b830e970f06cb990e6ba274c9845904
Unionindesign <stevemarshall89@yahoo.com>
2024-08-26 22:39:21 -0600

    updated a lot of old deps. and newest class schedules

--- src/components/schedule/schedule.jsx
+++ src/components/schedule/schedule.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./schedule.css";
 
-//React Materiliaze
+//React Materialiaze
 import { Row, Col } from "react-materialize";
 
 //images
 // import Corepower from "../../images/cpylogo.png";
-// import RaYogaLogo from "../../images/RaYogaLogo.gif";
-// import PurpleYogaLogo from "../../images/PurpleYogaLogo.png";
+import RaYogaLogo from "../../images/RaYogaLogo.gif";
+// import PurpleYogaLogo from "../../images/PurpleYogaLogo.png"; // <-- old Logo
+import PurpleYogaLogo from "../../images/PURPLE+YOGA_FINAL+2.png";
 // import Yogapod from "../../images/PurpleYogaLogo.png";
 //import LiveMusicYoga from "../../images/liveMusicYoga.png";
 // import ScheduleBackground from "../../images/cpyredrocks.png";
@@ -25,14 +26,14 @@ const Schedule = () => {
           <Row>
             {" "}
             <Col l={5} s={12}>
-            <div className="scheduleCard">
+            {/* <div className="scheduleCard">
                 <img
                   className="scheduleCardLogoImg"
                   src={logo}
                   alt="Michelle Scutti logo"
                 />
                 <p className="scheduleCardSpan">No public classes available at this time.</p>
-            </div>
+            </div> */}
                               
               {/* =========
                 COREPOWER
@@ -68,10 +69,59 @@ const Schedule = () => {
                 </div>
               </div>  */}
 
-              {/* =========
+             
+
+
+             {/* =========
+                PURPLE YOGA
+              ============= */}
+              
+              <div className="scheduleCard">
+                <img
+                  className="scheduleCardLogoImg"
+                  style={{ margin: "-20px auto -20px auto"  }}
+                  src={PurpleYogaLogo}
+                  alt="purple yoga logo"
+                />
+                <p className="scheduleCardSpan">
+                  <strong>Saturday - Long Beach </strong>
+                  <br />
+                  10:15am Blue 60 Vinyasa Yoga
+                  <br />
+                  11:30am Red 60 Heated Vinyasa Yoga
+                  <br />
+                </p>
+                <p className="scheduleCardSpan">
+                  <strong>Sunday - Huntington Beach </strong>
+                  <br />
+                  10:00am Red 60 Heated Vinyasa Yoga
+                  <br />
+                </p>
+                <div className="cardFooter">
+                  <hr />
+                  <a
+                    className="cardFooterLink"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.purpleyoga.org/fullerton" //TODO! Update link once the studio page is live
+                  >
+                    Huntington Beach
+                  </a>
+                  <a
+                    className="cardFooterLink"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.purpleyoga.org/long-beach"
+                  >
+                    Long Beach
+                  </a>
+                </div> 
+            </div>
+
+             {/* =========
                 RA YOGA
               ============= */}
-              {/* <div className="scheduleCard">
+              <div className="scheduleCard">
                 <img
                   className="scheduleCardLogoImg"
                   style={{ margin: "-20px auto -20px auto"  }}
@@ -81,68 +131,37 @@ const Schedule = () => {
                 <p className="scheduleCardSpan">
                   <strong>Hot Ra - Hot Yoga</strong>
                   <br />
-                  Wednesday 6:00am
-                  <br />
-                  Thursday 6:30am
+                  Subbing Costa Mesa, Newport Beach & Long Beach
                   <br />
                 </p>
                 <div className="cardFooter">
                   <hr />
+                  <a
+                    className="cardFooterLink"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://rayoga.com/costa-mesa-studio/"
+                  >
+                    Costa Mesa
+                  </a>
+                  <a
+                    className="cardFooterLink"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://rayoga.com/newport-beach-studio/"
+                  >
+                    Newport Beach
+                  </a>
                   <a
                     className="cardFooterLink"
                     target="_blank"
                     rel="noopener noreferrer"
                     href="https://rayoga.com/long-beach-studio/"
                   >
-                    Ra Yoga Long Beach Studio Info
+                    Long Beach
                   </a>
                 </div> 
-            </div> */}
-
-
-             {/* =========
-                PURPLE YOGA
-              ============= */}
-              
-              {/* <div className="scheduleCard">
-                <img
-                  className="scheduleCardLogoImg"
-                  style={{ margin: "-20px auto -20px auto"  }}
-                  src={PurpleYogaLogo}
-                  alt="purple yoga logo"
-                />
-                <p className="scheduleCardSpan">
-                  <strong>Blue 60 (Vinyasa) - Red 60 (Heated Vinyasa)</strong>
-                  <br />
-                  Monday Evening 6:30pm &  8:00pm Red 60 - Fullerton Studio
-                  <br />
-                  Wednesday 8:00pm Red 60 - Long Beach Studio
-                  <br />
-                  Saturday 8:30am Red 60 - Long Beach Studio
-                  <br />
-                  Saturday 10:15am Blue 60 - Long Beach Studio
-                  <br />
-                </p>
-                <div className="cardFooter">
-                  <hr />
-                  <a
-                    className="cardFooterLink"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://www.purpleyoga.org/fullerton"
-                  >
-                    Fullerton Studio
-                  </a>
-                  <a
-                    className="cardFooterLink"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://www.purpleyoga.org/long-beach"
-                  >
-                    Long Beach Studio
-                  </a>
-                </div> 
-            </div> */}
+            </div>
 
             </Col>
             <Col l={7} s={12}>
